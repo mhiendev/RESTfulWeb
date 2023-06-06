487ec68411e362b87e62fe020d50896f26ca5c9b
mhiendev <hienb1910641@student.ctu.edu.vn>
2023-06-06 11:21:55 +0700

    setup project include connect to MySQL using Hibernate

---
+++ src/main/java/com/appsdevblog/app/ws/App.java
@@ -0,0 +1,9 @@
+package com.appsdevblog.app.ws;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class App extends Application {
+
+}
